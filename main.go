a05568152adf49916c666eac3a61a70ae3fc9611
Greg Holmes <git@greg.holmes.name>
2023-03-01 23:22:12 +0000

    Add idle LED per gate in addition to cycle

--- main.go
+++ main.go
@@ -12,142 +12,148 @@ type Gate struct {
 	closedInput       machine.Pin
 	closedOutput      machine.Pin
 	openRequestOutput machine.Pin
+	idleOutput        machine.Pin
 }
 
 type Gates struct {
-	Gate1        *Gate
-	Gate2        *Gate
-	startingGate machine.Pin
-	endingGate   machine.Pin
-	debugLED     machine.Pin
+	outerGate       *Gate
+	innerGate       *Gate
+	inboundCycling  machine.Pin
+	outboundCycling machine.Pin
+	debugLED        machine.Pin
 }
 
 func main() {
 	outerGate := &Gate{
-		Name:              "1",
+		Name:              "Outer",
 		openRequestInput:  machine.D2,
 		stuckRequestInput: machine.NoPin,
 		closedInput:       machine.D4,
 		closedOutput:      machine.D5,
 		openRequestOutput: machine.D6,
+		idleOutput:        machine.ADC3,
 	}
 	innerGate := &Gate{
-		Name:              "2",
+		Name:              "Inner",
 		openRequestInput:  machine.D8,
 		stuckRequestInput: machine.NoPin,
 		closedInput:       machine.D10,
 		closedOutput:      machine.D11,
 		openRequestOutput: machine.D12,
+		idleOutput:        machine.ADC4,
 	}
 	gates := Gates{
-		Gate1:        outerGate,
-		Gate2:        innerGate,
-		startingGate: machine.D7,
-		endingGate:   machine.ADC0,
-		debugLED:     machine.LED,
+		outerGate:       outerGate,
+		innerGate:       innerGate,
+		inboundCycling:  machine.D7,
+		outboundCycling: machine.ADC0,
+		debugLED:        machine.LED,
 	}
 	gates.Init()
+	go func() {
+		for {
+			gates.setStatusLEDs()
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
 	for {
-		gates.setStatusLEDs()
+		//gates.setStatusLEDs()
 		if outerGate.isClosed() && innerGate.isClosed() {
 			if outerGate.checkOpenRequestInput() {
-				inbound := Gates{
-					Gate1:        outerGate,
-					Gate2:        innerGate,
-					startingGate: machine.D7,
-					endingGate:   machine.ADC0,
-					debugLED:     machine.LED,
-				}
-				inbound.Open()
+				gates.Inbound()
 			}
 			if innerGate.checkOpenRequestInput() {
-				outbound := Gates{
-					Gate1:        innerGate,
-					Gate2:        outerGate,
-					startingGate: machine.ADC0,
-					endingGate:   machine.D7,
-					debugLED:     machine.LED,
-				}
-				outbound.Open()
+				gates.Outbound()
 			}
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
 }
 
-func (gates *Gates) Open() {
+func (gates *Gates) Inbound() {
+	gates.inboundCycling.High()
+	gates.cycle(gates.outerGate, gates.innerGate)
+	gates.inboundCycling.Low()
+}
+
+func (gates *Gates) Outbound() {
+	gates.outboundCycling.High()
+	gates.cycle(gates.innerGate, gates.outerGate)
+	gates.outboundCycling.Low()
+}
+
+func (gates *Gates) cycle(gate1 *Gate, gate2 *Gate) {
 	ticks := 0
-	gates.setStatusLEDs()
-	gates.startingGate.High()
+	//gates.setStatusLEDs()
 	//Request first gate opens
-	gates.Gate1.requestOpen()
+	gate1.requestOpen()
 	//Wait for the gate to be open
-	for gates.Gate1.isClosed() {
-		gates.setStatusLEDs()
+	for gate1.isClosed() {
+		//gates.setStatusLEDs()
 		ticks++
 		//Timeout after about 5 seconds
 		if ticks > 50 {
-			gates.startingGate.Low()
+			gate1.idleOutput.High()
 			return
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
 	ticks = 0
 	//The first gate has started opening, wait for it to close again
-	for !gates.Gate1.isClosed() {
-		gates.setStatusLEDs()
+	for !gate1.isClosed() {
+		//gates.setStatusLEDs()
 		ticks++
 		//Timeout after about 2 minutes
 		if ticks > 120000 {
-			gates.startingGate.Low()
+			gate1.idleOutput.High()
 			return
 		}
-		gates.Gate1.openRequestOutput.Low()
 		time.Sleep(100 * time.Millisecond)
 	}
 	ticks = 0
 	//The first gate has closed, open the second gate
-	gates.Gate2.requestOpen()
+	gate1.idleOutput.High()
+	gate2.requestOpen()
 	//Wait for the second gate to open
-	for gates.Gate2.isClosed() {
-		gates.setStatusLEDs()
+	for gate2.isClosed() {
+		//gates.setStatusLEDs()
 		ticks++
 		//Timeout after about 5 seconds
 		if ticks > 50 {
-			gates.startingGate.Low()
+			gate2.idleOutput.High()
 			return
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
 	ticks = 0
 	//The second gate has opened, wait for it to close
-	for !gates.Gate2.isClosed() {
-		gates.setStatusLEDs()
+	for !gate2.isClosed() {
+		//gates.setStatusLEDs()
 		ticks++
 		//Timeout after about 2 minutes
 		if ticks > 120000 {
-			gates.startingGate.Low()
+			gate2.idleOutput.High()
 			return
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
 	//Second gate has closed, cycle complete
-	gates.startingGate.Low()
+	gate2.idleOutput.High()
 	return
 }
 
 func (gates *Gates) Init() {
-	gates.Gate1.Init()
-	gates.Gate2.Init()
-	gates.startingGate.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	gates.endingGate.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	gates.startingGate.Low()
-	gates.endingGate.Low()
+	gates.innerGate.Init()
+	gates.outerGate.Init()
+	gates.inboundCycling.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	gates.outboundCycling.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	gates.inboundCycling.Low()
+	gates.outboundCycling.Low()
 }
 
 func (gates *Gates) setStatusLEDs() {
-	gates.Gate1.closedOutput.Set(gates.Gate1.closedInput.Get())
-	gates.Gate2.closedOutput.Set(gates.Gate2.closedInput.Get())
+	gates.innerGate.closedOutput.Set(gates.innerGate.closedInput.Get())
+	gates.outerGate.closedOutput.Set(gates.outerGate.closedInput.Get())
 }
 
 func (g *Gate) Init() {
@@ -156,8 +162,10 @@ func (g *Gate) Init() {
 	g.closedInput.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
 	g.closedOutput.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	g.openRequestOutput.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	g.idleOutput.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	g.openRequestOutput.Low()
 	g.closedOutput.Low()
+	g.idleOutput.High()
 }
 
 func (g *Gate) isClosed() bool {
@@ -172,4 +180,5 @@ func (g *Gate) requestOpen() {
 	g.openRequestOutput.High()
 	time.Sleep(500 * time.Millisecond)
 	g.openRequestOutput.Low()
+	g.idleOutput.Low()
 }
